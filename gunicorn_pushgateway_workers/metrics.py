b8618605cda9dab6a8bf7751e48b370f691cc981
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-01-16 13:58:50 +0100

    Update metrics push

--- gunicorn_pushgateway_workers/metrics.py
+++ gunicorn_pushgateway_workers/metrics.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Any
 
 from prometheus_client import CollectorRegistry, multiprocess, push_to_gateway
 
 from gunicorn_pushgateway_workers.config import config
+
+logger = logging.getLogger(__name__)
 
 
 def get_registry() -> CollectorRegistry:
@@ -13,9 +16,12 @@ def get_registry() -> CollectorRegistry:
 
 
 def push(registry: CollectorRegistry, grouping_key: dict[str, Any]) -> None:
-    push_to_gateway(
-        gateway=config.HOST,
-        job=config.JOB,
-        registry=registry,
-        grouping_key=grouping_key,
-    )
+    try:
+        push_to_gateway(
+            gateway=config.HOST,
+            job=config.JOB,
+            registry=registry,
+            grouping_key=grouping_key,
+        )
+    except OSError as e:
+        logger.error(e)
