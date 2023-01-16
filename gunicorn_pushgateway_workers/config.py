1610c82a1a7abb6becc3a3ee4c49842c6aaf2105
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-01-16 13:49:13 +0100

    Update config

--- gunicorn_pushgateway_workers/config.py
+++ gunicorn_pushgateway_workers/config.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class Impl(str, Enum):
@@ -11,9 +11,15 @@ class Impl(str, Enum):
 class Config(BaseSettings):
     ENABLED: bool = False
     INTERVAL: int = 60
-    HOST: str
-    JOB: str
+    HOST: str = ""
+    JOB: str = ""
     IMPL: Impl = Impl.THREAD
+
+    @validator("HOST", "JOB")
+    def reqired_if_enabled(cls, v, values):
+        if values["ENABLED"] and not v:
+            raise ValueError("Required")
+        return v
 
     class Config:
         env_prefix = "PUSH_GETAWAY_"
